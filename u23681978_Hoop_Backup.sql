9fab58a79dff64cb712aa77079b1a053c3262529
David Musa-Aisien <cal24815@gmail.com>
2024-05-21 21:40:27 +0200

    Add files via upload

---
+++ u23681978_Hoop_Backup.sql
@@ -0,0 +1,320 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4deb2~bpo10+1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: May 21, 2024 at 09:39 PM
+-- Server version: 10.3.39-MariaDB-0+deb10u2
+-- PHP Version: 7.3.31-1~deb10u5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `u23681978_Hoop_Backup`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Actor`
+--
+
+CREATE TABLE `Actor` (
+  `Firstname` varchar(50) NOT NULL,
+  `ActorID` int(11) NOT NULL,
+  `Lastname` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `Actor`
+--
+
+INSERT INTO `Actor` (`Firstname`, `ActorID`, `Lastname`) VALUES
+('Ryan', 7, 'Reynolds'),
+('Tom', 8, 'Holland');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Crew`
+--
+
+CREATE TABLE `Crew` (
+  `CrewID` int(11) NOT NULL,
+  `Role` varchar(50) NOT NULL,
+  `Firstname` varchar(50) NOT NULL,
+  `Lastname` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Movies`
+--
+
+CREATE TABLE `Movies` (
+  `Title` varchar(100) NOT NULL,
+  `MovieID` int(11) NOT NULL,
+  `Rating` double NOT NULL,
+  `Runtime` time NOT NULL,
+  `AgeRating` varchar(20) NOT NULL,
+  `CrewID` int(11) DEFAULT NULL,
+  `ReleaseYear` year(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Movie_Actor`
+--
+
+CREATE TABLE `Movie_Actor` (
+  `MovieID` int(11) NOT NULL,
+  `ActorID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Movie_Reviews`
+--
+
+CREATE TABLE `Movie_Reviews` (
+  `UserID` int(11) NOT NULL,
+  `MovieID` int(11) NOT NULL,
+  `Review` varchar(300) NOT NULL,
+  `StarRating` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Recommendations`
+--
+
+CREATE TABLE `Recommendations` (
+  `UserID` int(11) NOT NULL,
+  `Genre` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Series`
+--
+
+CREATE TABLE `Series` (
+  `SeriesID` int(11) NOT NULL,
+  `CrewID` int(11) DEFAULT NULL,
+  `Title` varchar(100) NOT NULL,
+  `ReleaseYear` year(4) NOT NULL,
+  `AgeRating` varchar(50) NOT NULL,
+  `Rating` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Series_Actor`
+--
+
+CREATE TABLE `Series_Actor` (
+  `SeriesID` int(11) NOT NULL,
+  `ActorID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Series_Reviews`
+--
+
+CREATE TABLE `Series_Reviews` (
+  `UserID` int(11) NOT NULL,
+  `SeriesID` int(11) NOT NULL,
+  `Review` varchar(300) NOT NULL,
+  `StarRating` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Series_Season`
+--
+
+CREATE TABLE `Series_Season` (
+  `SeriesID` int(11) NOT NULL,
+  `Season` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Subscription`
+--
+
+CREATE TABLE `Subscription` (
+  `UserID` int(11) NOT NULL,
+  `Newsletter` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `User`
+--
+
+CREATE TABLE `User` (
+  `UserID` int(11) NOT NULL,
+  `Email` varchar(100) NOT NULL,
+  `Age` int(11) NOT NULL,
+  `Firstname` varchar(50) NOT NULL,
+  `Lastname` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Actor`
+--
+ALTER TABLE `Actor`
+  ADD PRIMARY KEY (`ActorID`);
+
+--
+-- Indexes for table `Crew`
+--
+ALTER TABLE `Crew`
+  ADD PRIMARY KEY (`CrewID`);
+
+--
+-- Indexes for table `Movies`
+--
+ALTER TABLE `Movies`
+  ADD PRIMARY KEY (`MovieID`);
+
+--
+-- Indexes for table `Movie_Actor`
+--
+ALTER TABLE `Movie_Actor`
+  ADD PRIMARY KEY (`MovieID`,`ActorID`);
+
+--
+-- Indexes for table `Movie_Reviews`
+--
+ALTER TABLE `Movie_Reviews`
+  ADD PRIMARY KEY (`UserID`,`MovieID`);
+
+--
+-- Indexes for table `Recommendations`
+--
+ALTER TABLE `Recommendations`
+  ADD PRIMARY KEY (`UserID`,`Genre`);
+
+--
+-- Indexes for table `Series`
+--
+ALTER TABLE `Series`
+  ADD PRIMARY KEY (`SeriesID`);
+
+--
+-- Indexes for table `Series_Actor`
+--
+ALTER TABLE `Series_Actor`
+  ADD PRIMARY KEY (`SeriesID`,`ActorID`);
+
+--
+-- Indexes for table `Series_Reviews`
+--
+ALTER TABLE `Series_Reviews`
+  ADD PRIMARY KEY (`UserID`,`SeriesID`);
+
+--
+-- Indexes for table `Series_Season`
+--
+ALTER TABLE `Series_Season`
+  ADD PRIMARY KEY (`SeriesID`,`Season`);
+
+--
+-- Indexes for table `Subscription`
+--
+ALTER TABLE `Subscription`
+  ADD PRIMARY KEY (`UserID`);
+
+--
+-- Indexes for table `User`
+--
+ALTER TABLE `User`
+  ADD PRIMARY KEY (`UserID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Actor`
+--
+ALTER TABLE `Actor`
+  MODIFY `ActorID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `Crew`
+--
+ALTER TABLE `Crew`
+  MODIFY `CrewID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Movies`
+--
+ALTER TABLE `Movies`
+  MODIFY `MovieID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Series`
+--
+ALTER TABLE `Series`
+  MODIFY `SeriesID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `User`
+--
+ALTER TABLE `User`
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Movies`
+--
+ALTER TABLE `Movies`
+  ADD CONSTRAINT `MovieActor` FOREIGN KEY (`MovieID`) REFERENCES `Movie_Actor` (`MovieID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `Series`
+--
+ALTER TABLE `Series`
+  ADD CONSTRAINT `SeriesActor` FOREIGN KEY (`SeriesID`) REFERENCES `Series_Actor` (`SeriesID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `User`
+--
+ALTER TABLE `User`
+  ADD CONSTRAINT `MovieReview` FOREIGN KEY (`UserID`) REFERENCES `Movie_Reviews` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `SeriesReview` FOREIGN KEY (`UserID`) REFERENCES `Series_Reviews` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `Subscription` FOREIGN KEY (`UserID`) REFERENCES `Subscription` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
